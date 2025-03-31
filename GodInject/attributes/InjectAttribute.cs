8fd339eed3d6ed5be4cad10eec6d494ad3e72a32
Vulpi <78976654+imvulpi@users.noreply.github.com>
2025-03-31 11:58:37 +0200

    Service key attribute fix for NET8.0
    Fixed the service key property in the inject attribute for NET8.0
    The main constructor user in NET8.0 in the Inject attribute failed to make the service key null, which resulted in the service key always being "" - which is a valid service key.
    
    Removed the NET8.0 main constructor, now all versions use the same implementation of [Inject]
    
    Version: 1.0.3

--- GodInject/attributes/InjectAttribute.cs
+++ GodInject/attributes/InjectAttribute.cs
@@ -2,21 +2,14 @@
 
 namespace GodInject
 {
-#if NET8_0_OR_GREATER
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-    public class InjectAttribute(string key = null) : Attribute
-    {
-        public string Key { get; } = key;
-    }
-#else
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class InjectAttribute : Attribute
     {
-        public InjectAttribute(){}
-        public InjectAttribute(string key){
+        public InjectAttribute() { }
+        public InjectAttribute(string key)
+        {
             Key = key;
         }
         public string Key { get; set; }
     }
-#endif
 }
